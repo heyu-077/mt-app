d319d0c8efdd62377d11ed643aec7c68535baf2c
heyu-077 <1834617305@qq.com>
2020-12-29 20:46:14 +0800

    项目搭建完成和网页头部的实现

---
+++ mt-app/src/components/header/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="header">
+        <el-row>
+            <top-bar />
+        </el-row>
+        <el-row>
+            <search-bar />
+        </el-row>
+    </div>
+</template>
+
+<script>
+import TopBar from './topBar.vue'
+import SearchBar from './searchBar.vue'
+
+export default {
+  components: {
+    TopBar,
+    SearchBar
+  }
+}
+</script>
---
+++ mt-app/src/components/header/topBar.vue
@@ -0,0 +1,26 @@
+<template>
+    <el-row class="m-header m-header-topbar">
+        <el-col :span="10">
+            <geo />
+         </el-col>
+         <el-col :span="14">
+            <my-nav />
+         </el-col>
+    </el-row>
+</template>
+
+<script>
+import geo from './geo.vue';
+import MyNav from './nav.vue';
+
+export default {
+     components:{
+         geo,
+         MyNav
+     }
+}
+</script>
+
+<style lang="scss">
+   @import '@/assets/css/public/header/topbar.scss';
+</style>
